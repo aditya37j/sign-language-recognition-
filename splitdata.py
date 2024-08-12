4873c9bdc69a9d26df44a0108bc24a64bc0bb772
aditya37j <149615000+aditya37j@users.noreply.github.com>
2024-08-12 20:24:39 +0530

    Add files via upload

---
+++ splitdata.py
@@ -0,0 +1,8 @@
+#SPLITTING DATASET INTO TRAINING AND TESTING 
+import splitfolders
+dr ='dataset48x48'
+
+splitfolders.ratio(dr,"splitdataset48x48",ratio=(0.8,0.2))
+
+#SPLITTING DONE INTO THE SPLITDATASET48*48 FILE 
+
